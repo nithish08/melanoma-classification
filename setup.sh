382f3ef2bc3b9aed1b71bdf0fd8c53e5fbe9091f
nithish08 <nithishreddy777@gmail.com>
2020-07-17 02:36:58 -0700

    remove

--- setup.sh
+++ setup.sh
@@ -5,10 +5,3 @@ conda activate python3
 pip install --upgrade pip
 pip install fastai2
 pip install --upgrade kaggle
-mkdir ~/.kaggle
-echo "{"username":"nithish007","key":"3f8c1f608fe1ed3f217cc1ca3d857134"}" > ~/.kaggle/kaggle.json
-chmod 600 ~/.kaggle/kaggle.json
-export KAGGLE_USERNAME=nithish007
-export KAGGLE_KEY="3f8c1f608fe1ed3f217cc1ca3d857134"
-git config --global user.name "nithish08"
-git config --global user.email "nithishreddy777@gmail.com"
